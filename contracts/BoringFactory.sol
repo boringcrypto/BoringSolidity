4a9bc36a6b3fb1622540960bc89348b2e009fd02
pinkiebell <40266861+pinkiebell@users.noreply.github.com>
2021-02-12 12:57:27 +0100

    BoringFactory: simplify inline asm

--- contracts/BoringFactory.sol
+++ contracts/BoringFactory.sol
@@ -72,17 +72,16 @@ contract BoringFactory {
             53:JUMPDEST(0x5b)
             54:RETURN(0xf3)
             */
-            let clone := mload(0x40)
-            mstore(clone, 0x3d602d80600a3d3981f3363d3d373d3d3d363d73000000000000000000000000)
-            mstore(add(clone, 0x14), targetBytes)
-            mstore(add(clone, 0x28), 0x5af43d82803e903d91602b57fd5bf30000000000000000000000000000000000)
+            mstore(0, 0x3d602d80600a3d3981f3363d3d373d3d3d363d73000000000000000000000000)
+            mstore(0x14, targetBytes)
+            mstore(0x28, 0x5af43d82803e903d91602b57fd5bf30000000000000000000000000000000000)
 
             switch useCreate2
             case 0 {
-                cloneAddress := create(0, clone, 0x37)
+                cloneAddress := create(0, 0, 0x37)
             }
             default {
-                cloneAddress := create2(0, clone, 0x37, salt)
+                cloneAddress := create2(0, 0, 0x37, salt)
             }
         }
 
