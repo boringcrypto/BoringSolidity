6ac991c15188c6c09d50944c489debfa8797388f
BoringCrypto <b@rtje.net>
2021-03-01 20:35:57 +1100

    Deal with safeName/Symbol returned as bytes32

--- contracts/libraries/BoringERC20.sol
+++ contracts/libraries/BoringERC20.sol
@@ -11,12 +11,30 @@ library BoringERC20 {
     bytes4 private constant SIG_TRANSFER = 0xa9059cbb; // transfer(address,uint256)
     bytes4 private constant SIG_TRANSFER_FROM = 0x23b872dd; // transferFrom(address,address,uint256)
 
+    function returnDataToString(bytes memory data) internal pure returns (string memory) {
+        if (data.length >= 64) {
+            return abi.decode(data, (string));
+        } else if (data.length == 32) {
+            uint8 i = 0;
+            while(i < 32 && data[i] != 0) {
+                i++;
+            }
+            bytes memory bytesArray = new bytes(i);
+            for (i = 0; i < 32 && data[i] != 0; i++) {
+                bytesArray[i] = data[i];
+            }
+            return string(bytesArray);
+        } else {
+            return "???";
+        }
+    }
+
     /// @notice Provides a safe ERC20.symbol version which returns '???' as fallback string.
     /// @param token The address of the ERC-20 token contract.
     /// @return (string) Token symbol.
     function safeSymbol(IERC20 token) internal view returns (string memory) {
         (bool success, bytes memory data) = address(token).staticcall(abi.encodeWithSelector(SIG_SYMBOL));
-        return success && data.length > 0 ? abi.decode(data, (string)) : "???";
+        return success ? returnDataToString(data) : "???";
     }
 
     /// @notice Provides a safe ERC20.name version which returns '???' as fallback string.
@@ -24,7 +42,7 @@ library BoringERC20 {
     /// @return (string) Token name.
     function safeName(IERC20 token) internal view returns (string memory) {
         (bool success, bytes memory data) = address(token).staticcall(abi.encodeWithSelector(SIG_NAME));
-        return success && data.length > 0 ? abi.decode(data, (string)) : "???";
+        return success ? returnDataToString(data) : "???";
     }
 
     /// @notice Provides a safe ERC20.decimals version which returns '18' as fallback value.
